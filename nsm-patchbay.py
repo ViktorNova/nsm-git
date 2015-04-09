b687d02023eb5843364897b3742a131a914a50d8
ViktorNova <viktor@pinknoiseboys.com>
2015-04-09 12:42:30 -0700

    Refactored some variable names

--- nsm-patchbay.py
+++ nsm-patchbay.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python2
-#Patchbay daemon for NSM
+# Patchbay daemon for NSM
 #NSM code based on rhetr's nsm-git
 #Easier stuff by Viktor Nova
 import liblo, sys, os, subprocess
@@ -7,8 +7,11 @@ import atexit
 from signal import signal, SIGTERM
 #from subprocess import call
 
-class NSMPatchbay(liblo.Server):
+class StagePatch(liblo.Server):
     def __init__(self):
+        """
+        :rtype : object
+        """
         liblo.Server.__init__(self)
         self.add_method("/reply", 'ssss', self.handshake_callback)
         self.add_method("/nsm/client/open", 'sss', self.open_callback)
@@ -18,16 +21,16 @@ class NSMPatchbay(liblo.Server):
         self.add_method("/reply", 'ss', self.server_save_callback)
         self.add_method("/error", 'sis', self.error_callback)
         self.add_method(None, None, self.fallback)
-        
+
         self.session_dir = None
         self.server_saved = False
         self.exit = False
         self.app = None
-        
+
         self.saveFile = None
         self.pid = None
         self.gui_pid = None
-        
+
         self.NSM_URL = os.getenv('NSM_URL')
         # self.NSM_URL = "osc.udp://datakTARR:10001/" # for testing purposes
         if not self.NSM_URL:
@@ -35,11 +38,11 @@ class NSMPatchbay(liblo.Server):
             sys.exit()
 
         self.handshake()
-        
+
         self.aj_snapshot()
 
 
-# ------------------------ AJ-Snapshot ----------------------------
+    # ------------------------ AJ-Snapshot ----------------------------
     def aj_snapshot(self):
         # Sets the name of the aj-snapshot file
         self.saveFile = os.path.join(self.session_dir, 'stagepatch.xml')
@@ -49,8 +52,8 @@ class NSMPatchbay(liblo.Server):
         if not os.path.isfile(self.saveFile):
             print "First run, creating new patchbay file from current MIDI and JACK connections to %s" % (self.saveFile)
             subprocess.call(["aj-snapshot", self.saveFile],
-                             stdout=subprocess.PIPE,
-                             preexec_fn=os.setsid)
+                            stdout=subprocess.PIPE,
+                            preexec_fn=os.setsid)
 
         print "Removing existing connections"
         print "Restoring connections from" + self.saveFile
@@ -58,29 +61,30 @@ class NSMPatchbay(liblo.Server):
         self.daemon = subprocess.Popen(["aj-snapshot", "-dx", self.saveFile],
                                        stdout=subprocess.PIPE,
                                        preexec_fn=os.setsid)
-        self.pid = self.daemon.pid                               
+        self.pid = self.daemon.pid
         print "Started patchbay daemon with pid " + repr(self.pid)
-        
+
         def cleanup(self, reason):
             print "Cleaning up.."
             print "Killing aj-snapshot system-wide"
             print "FIXME! Killing all instances of aj-snapshot is a dity hack and NOT the solution!"
-            """
-            if self.pid:
-                print "aj-snapshot is running"
-            else:
-                print "aj-snapshot AINT running"
-            if self.app.pid:
-                print "GUI is running"
-            else:
-                print "GUI AINT running"
-            """
+
+            #    if self.pid:
+            #        print "aj-snapshot is running"
+            #    else:
+            #        print "aj-snapshot AINT running"
+            #    if self.app.pid:
+            #        print "GUI is running"
+            #    else:
+            #        print "GUI AINT running"
+
+            # FIX THIS!!! FIX THIS!!! FIX THIS!!!
             os.system('killall -KILL aj-snapshot')
-            
+
             sys.exit()
-        
+
         signal(SIGTERM, cleanup)
-        
+
     # ---------------------------------------------------------------------
     # callbacks
 
@@ -89,14 +93,15 @@ class NSMPatchbay(liblo.Server):
 
     def open_callback(self, path, args):
         self.session_dir, self.display_name, self.client_id = args
-        self.session_dir = os.path.split(self.session_dir)[0]        
+        self.session_dir = os.path.split(self.session_dir)[0]
         self.save()
         message = liblo.Message('/reply', "/nsm/client/open", 'done')
         liblo.send(self.NSM_URL, message)
 
     def save_callback(self, path, args):
-        self.server_saved = False #Viktor changed this from false.. might need to change it back?
-        message = liblo.Message('/reply', "/nsm/client/save", 'NOT saved - By design NSM Patchbay must be saved manually from the GUI')
+        self.server_saved = False  #Viktor changed this from false.. might need to change it back?
+        message = liblo.Message('/reply', "/nsm/client/save",
+                                'NOT saved - By design NSM Patchbay must be saved manually from the GUI')
         liblo.send(self.NSM_URL, message)
         print 'NOT saved - NSM Patchbay must be saved manually from the GUI'
         saved = self.save()
@@ -114,7 +119,8 @@ class NSMPatchbay(liblo.Server):
             print 'no session open'
             self.exit = True
 
-# ----------------------- SHOW GUI ----------------------------------
+        # ----------------------- SHOW GUI ----------------------------------
+
     def show_gui_callback(self, path, args):
         if self.app:
             if self.app.poll() == 0:
@@ -124,18 +130,17 @@ class NSMPatchbay(liblo.Server):
 
         if not self.app:
             self.app = subprocess.Popen([os.path.join(self.executable_dir, 'stagepatch-gui.py'),
-                                         self.saveFile, 
+                                         self.saveFile,
                                          repr(self.pid)],
-                                         stdout=subprocess.PIPE,
-                                         preexec_fn=os.setsid)
+                                        stdout=subprocess.PIPE,
+                                        preexec_fn=os.setsid)
             self.gui_pid = self.app.pid
             print 'Showing gui', self.gui_pid
-            
+
             # Pipe all output from the GUI subprocess and show it on the console
             gui_output = iter(self.app.stdout.readline, b"")
             for line in gui_output:
-                print(line) # yield line
-        
+                print(line)  # yield line
 
 
     def hide_gui_callback(self, path, args):
@@ -153,16 +158,17 @@ class NSMPatchbay(liblo.Server):
     # internal methods
 
     def handshake(self):
-        application_name = "nsm-patchbay"
+        application_name = "Stagepatch"
         capabilities = ":message:optional-gui:"
         executable_name = os.path.realpath(__file__)
         self.executable_dir = os.path.dirname(executable_name)
-        executable_name = "nsm-patchbay.py" # this leads to mostly correct behaviour, I don't know how to find the actual 'correct' value though
+        executable_name = "nsm-patchbay.py"  # this leads to mostly correct behaviour, I don't know how to find the actual 'correct' value though
         pid = os.getpid()
         api_version_major = 1
         api_version_minor = 2
 
-        message = liblo.Message("/nsm/server/announce", application_name, capabilities, executable_name, api_version_major, api_version_minor, pid)
+        message = liblo.Message("/nsm/server/announce", application_name, capabilities, executable_name,
+                                api_version_major, api_version_minor, pid)
         liblo.send(self.NSM_URL, message)
 
         while not self.session_dir:
@@ -172,31 +178,27 @@ class NSMPatchbay(liblo.Server):
 
     # ---------------------------------------------------------------------
     # save methods
-        
+
     def init_repo(self):
         print "FIXME: def init_repo(self) is being called"
         print "Move the initial stuff to here instead of the handshake"
-        
+
 
     def save(self):
         print "Fix me: def save is being called."
         print "This happens when NSM asks the client to save, which is good, but it also happens when NSM opens for the first time, which makes sense for the code I forked this from, but not for typical stuff"
         return True
-        
 
 # --------------------- CLEANUP ON SHUTDOWN ------------------------
-
 try:
 
-
-    nsm_git = NSMPatchbay()
+    stage_patch = StagePatch()
 except liblo.ServerError, err:
-    ## Debug Quazarrr
+    # Debug Quazarrr
     print "Debug Quazarrr"
-    
+
     print str(err)
     # cleanup() Figure out how to call cleanup from here too, in case of an error
 
-
 while True:
-    nsm_git.recv(100) 
+    stage_patch.recv(100)
